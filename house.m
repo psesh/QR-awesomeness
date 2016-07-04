b4c228fba3dacf176ad46fd29978bd32ba721837
Pranay Seshadri <ps583@cam.ac.uk>
2016-07-04 20:31:03 +0100

    Create house.m

---
+++ house.m
@@ -0,0 +1,26 @@
+% Computation of the Householder vector. For an input x in R^(m x 1), t
+% this function computes v in R^(m x 1) with v(1) = 1 and a constant betav 
+% such that P = eye(m) - betav*v*v' is orthogonal. 
+%
+% Copyright (c) 2016 by Pranay Seshadri
+function [v,betav] = house(x)
+
+m = length(x);
+sigmav = x(2:m)' * x(2:m);
+v = [1; x(2:m) ];
+
+
+if(sigmav == 0 && x(1) >= 0 )
+    betav = 0;
+elseif(sigmav == 0 && x(1) < 0 )
+    betav = -2;
+else
+    muv = sqrt(x(1)^2 + sigmav);
+    if x(1) <= 0
+        v(1) = x(1) - muv;
+    else
+        v(1) = -sigmav/(x(1) + muv) ;
+    end
+    betav = 2*v(1)^2 / (sigmav + v(1)^2);
+    v = v/v(1);
+end
