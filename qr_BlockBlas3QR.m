2df045419375c0f23f65530e7b4bb8f18416bb68
Pranay Seshadri <pranay.seshadri@vanderbilt.edu>
2016-07-05 22:45:20 +0100

    Index exceeds dimensions issue.

--- qr_BlockBlas3QR.m
+++ qr_BlockBlas3QR.m
@@ -43,20 +43,22 @@ for j = 1 : nb
     end
     
     % Update pivot column
-    Asub(k:m,j) = Asub(k:m,j) - Asub(k:m, 1:j-1) * F(k, 1:j-1)';
+    size(F)
+    size(Asub)
+    Asub(k:m,j) = Asub(k:m,j) - Asub(k:m, 1:j-1) * F(1, 1:j-1)';
+    disp('got here');
     
     % Reduction
     [v, beta_v] = house(Asub(k:m, j));
     tau(j) = beta_v;
     Y(:,j) = v;
-    Hj = eye(m-k+1) - tau(j) * Y(:,j) * Y(:,j)';
     
     % Incremental computation of F:
     F(j+1:n,j) = tau(j) * Asub(j:m,j+1:n)' * Y(j:m,j);
     F(1:n, j) = F(1:n,j) - tau(j) * F(1:n, 1:j-1) * Y(j:m, 1:j-1)' * Y(j:m,j);
     
     % Update of pivot row
-    Asub(k,j+1:n) = Asub(k,j+1:n) - Y(k,1:j) * F(j+1:n, 1:j)';
+    Asub(k,j+1:n) = Asub(k,j+1:n) - Asub(k,1:j) * F(j+1:n, 1:j)';
     
     % Norm downdate
     column_norms(j+1:n) = column_norms(j+1:n) - Asub(k, j+1: n).^2;
