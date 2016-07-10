a2b21e5756058f415f1562225824b19fc47b06bc
Pranay Seshadri <ps583@cam.ac.uk>
2016-07-10 11:08:33 -0400

    Update qr_BlockHouseholder.m

--- Businger-Golub/qr_BlockHouseholder.m
+++ Businger-Golub/qr_BlockHouseholder.m
@@ -24,5 +24,24 @@ while lambda <= n
     Q(:, lambda:m) = Q(:, lambda:m) * (eye(rows,cols) - W * Y');
     lambda = tau + 1;   
 end
+end
+
+function [v_store, betav_store] = qr_Householder_basic(A)
+
+% Size of A
+[m,n] = size(A);
+
+for j = 1 : n - 1
+    [v,betav] = house(A(j:m,j));
+    H = (eye(m-j+1) - betav * (v * v') ); % I'd prefer not to use H{j}!
+    A(j:m,j:n) =  H * A(j:m,j:n);
+    if j < m
+        A(j+1:m,j) = v(2:m - j + 1);
+    end
+    
+    % Store
+    v_store(:,j) = [zeros(j-1,1) ; v];
+    betav_store(j) = betav;
+end
 
 end
